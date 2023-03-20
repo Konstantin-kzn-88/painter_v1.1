6d3ae8c50b38066d1ca78e9fdd5dff4f960d82cc
konstantin <kuznetsovkm@yandex.ru>
2023-03-20 21:32:02 +0300

    Настройка pcolor шкалы

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import numpy as np
 from scipy.signal import fftconvolve
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def fmt(x, pos):
+    a, b = '{:.2e}'.format(x).split('e')
+    b = int(b)
+    return r'${} \times 10^{{{}}}$'.format(a, b)
 
 points = np.array([[150, 150], [90, 90], [250, 330], [400, 300], [100, 250], [400, 100], [250, 400]])
 width, height = 500, 500
@@ -14,4 +20,7 @@ power = 1 - distance / 100
 power[power < 0] = 0  # cut off power function
 power = power / np.sum(power)  # normalization
 result = fftconvolve(im, power, mode='same')  # convolutions
-plt.imshow(result, cmap='jet', aspect='equal')
+plt.pcolor(result, cmap='jet')
+plt.colorbar(format=ticker.FuncFormatter(fmt))
+plt.savefig('foo.png')
+
