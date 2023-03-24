e93f08c31db33e2f586b475152ea17042a61d571
konstantin <kuznetsovkm@yandex.ru>
2023-03-24 19:46:42 +0300

    Расчет риска классом

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-import numpy as np
-from scipy.signal import fftconvolve
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from risk import risk_allocation
+
 
 def fmt(x, pos):
     a, b = '{:.2e}'.format(x).split('e')
     b = int(b)
     return r'${} \times 10^{{{}}}$'.format(a, b)
 
-points = np.array([[150, 150], [90, 90], [250, 330], [400, 300], [100, 250], [400, 100], [250, 400]])
-width, height = 500, 500
-im = np.zeros((width, height))
-im[points[:, 1], points[:, 0]] = 1.0  # set points
 
-x = np.arange(0, 201)
-y = np.arange(0, 201)
-X, Y = np.meshgrid(x, y)
-print(X, Y)
-distance = ((X - 100) ** 2 + (Y - 100) ** 2) ** .5
-power = 1 - distance / 100
-power[power < 0] = 0  # cut off power function
-power = power / np.sum(power)  # normalization
-result = fftconvolve(im, power, mode='same')  # convolutions
+points = [[150, 150], [90, 90], [250, 330], [400, 300], [100, 250], [400, 100], [250, 400]]
+size_area = (500, 500)
+power_risk = 100
+result = 0
+
+for point in points:
+    result += risk_allocation.Allocation(point, size_area, power_risk).calculation()
+
 plt.pcolor(result, cmap='jet')
 plt.colorbar(format=ticker.FuncFormatter(fmt))
-plt.savefig('foo.png')
-
+plt.show()
---
+++ risk/risk_allocation.py
@@ -0,0 +1,43 @@
+# -----------------------------------------------------------
+# Класс предназначен для расчета распредления риска
+#
+# (C) 2023 Kuznetsov Konstantin, Kazan , Russian Federation
+# email kuznetsovkm@yandex.ru
+# -----------------------------------------------------------
+
+import numpy as np
+from scipy.signal import fftconvolve
+
+class Allocation:
+    """
+    Класс предназначен для расчета распредления риска
+    """
+
+    def __init__(self, points: list, size_area: tuple, power_risk: int):
+        '''
+
+        :@param points: список точек 2-D ( например, [[x,y],[x1,y1],...,[xn,yn] )
+        :@param size_area: размер исследуемой области, pixel (например, (width, height))
+        :@param power_risk: сила воздействия точек, pixel
+        '''
+        self.points = points
+        self.size_area = size_area
+        self.power_risk = power_risk
+
+    def calculation(self):
+        points = np.array([self.points, ])
+        width, height =  self.size_area
+        im = np.zeros((width, height))
+        im[points[:, 1], points[:, 0]] = 1.0  # set points
+
+        x = np.arange(0, self.power_risk*2)
+        y = np.arange(0, self.power_risk*2)
+        X, Y = np.meshgrid(x, y)
+        distance = ((X - 100) ** 2 + (Y - 100) ** 2) ** .5
+        power = 1 - distance / 100
+        power[power < 0] = 0  # cut off power function
+        power = power / np.sum(power)  # normalization
+        result = fftconvolve(im, power, mode='same')  # convolutions
+
+        return  result
+
