cba52ce32244c2c39906a01dd892f2306df82fbd
Konstantin <kuznetsovkm@yandex.ru>
2023-03-29 17:47:40 +0300

    Выделил нужные участки для поиска

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ from PySide6.QtCore import QObject, Signal, QRunnable, QThreadPool
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # Координаты объектов
-OBJECTS = [(10, 10), (2500, 2500), (500, 2500), (2500, 2700, 1000, 1500), (300, 300, 300, 500, 500, 500, 500, 300)]
+# OBJECTS = [(10, 10), (2500, 2500), (500, 2500), (2500, 2700, 1000, 1500), (300, 300, 300, 500, 500, 500, 500, 300)]
+OBJECTS = [(10, 10), ]
 # Типы объектов
 # 0 - линия
 # 1 - многоугольник
 # 2 - точка
 TYPE_OBJECTS = [2, 2, 2, 0, 1]
 # Площадь картинки
-SIZE_AREA = (3628, 3211)
+SIZE_AREA = (628, 211)
 # Сила риска для каждого объекта
 POWER = [100, 100, 100, 100, 100]
 # Размер анализируемой сетки
@@ -35,21 +36,44 @@ class WorkerSignals(QObject):
 
 
 class Worker(QRunnable):
-    def __init__(self, size_area: tuple, obj_points: tuple, obj_type: int):
+    def __init__(self, size_area: tuple, obj_points: tuple, obj_type: int, power: int):
         super().__init__()
         self.signals = WorkerSignals()
         self.size_area = size_area
         self.obj_points = obj_points
         self.obj_type = obj_type
+        self.power = power
 
     def run(self):
         try:
+            # Создадим объект до которого идет измерение
+            if len(self.obj_points) > 2:
+                if self.obj_type == 0:  # линейный объект
+                    print('линейный')
+                    obj = LineString(list(zip(self.obj_points[0::2], self.obj_points[1::2])))
+                else:
+                    print('стационарный')
+                    obj = Polygon(list(zip(self.obj_points[0::2], self.obj_points[1::2])))
+            else:
+                print('точка')
+                obj = Point(self.obj_points[0], self.obj_points[1])
             # посмотрим к каким областям относится рассматриваемы объект
             # разобьем площадь картинки на квадраты поиска на квадраты поиска
-            x_line = [i for i in range(0, self.size_area[0] + 1, SIZE_SEARCH)].append(self.size_area[0]%SIZE_SEARCH)
-            y_line = [i for i in range(0, self.size_area[1] + 1, SIZE_SEARCH)].append(self.size_area[1]%SIZE_SEARCH)
+            x_line = [i for i in range(0, self.size_area[0] + 1, SIZE_SEARCH)]
+            y_line = [i for i in range(0, self.size_area[1] + 1, SIZE_SEARCH)]
 
-            print(f'Объект: {self.obj_points}')
+            find_coordinate = []
+
+            for x in x_line:
+                for y in y_line:
+                    search_point = (x, y, x + SIZE_SEARCH, y, x + SIZE_SEARCH, y + SIZE_SEARCH, x, y + SIZE_SEARCH)
+                    search_polygon = Polygon(list(zip(search_point[0::2], search_point[1::2])))
+                    distance = search_polygon.distance(obj)
+                    if distance < self.power:
+                        find_coordinate.extend(search_point)
+
+
+            print(f'Координаты: {find_coordinate}')
             time.sleep(3)
         except Exception as e:
             self.signals.error.emit(str(e))
@@ -73,7 +97,7 @@ class MainWindow(QMainWindow):
 
     def start_worker(self):
         for OBJECT in OBJECTS:
-            worker = Worker(SIZE_AREA, OBJECT, POWER[OBJECTS.index(OBJECT)])
+            worker = Worker(SIZE_AREA, OBJECT, TYPE_OBJECTS[OBJECTS.index(OBJECT)], POWER[OBJECTS.index(OBJECT)])
             worker.signals.result.connect(self.worker_output)
             worker.signals.finished.connect(self.worker_complete)
             self.threadpool.start(worker)
