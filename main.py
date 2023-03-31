499eff5ef65afb21a5fef2543e1a43fed91c9278
konstantin <kuznetsovkm@yandex.ru>
2023-03-31 22:42:49 +0300

    ...

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from PySide6.QtCore import QObject, Signal, QRunnable, QThreadPool
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # Координаты объектов
-# OBJECTS = [(10, 10), (2500, 2500), (500, 2500), (2500, 2700, 1000, 1500), (300, 300, 300, 500, 500, 500, 500, 300)]
-OBJECTS = [(10, 10), ]
+OBJECTS = [(500, 100), (400, 200), (250, 500), (100, 150, 300, 300), (500, 500, 500, 400, 400, 400, 400, 500)]
+
 # Типы объектов
 # 0 - линия
 # 1 - многоугольник
@@ -17,7 +17,7 @@ OBJECTS = [(10, 10), ]
 TYPE_OBJECTS = [2, 2, 2, 0, 1]
 # Площадь картинки
 SIZE_AREA = (628, 628)
-# Сила риска для каждого объекта
+# Сила воздействия для каждого объекта
 POWER = [100, 100, 100, 100, 100]
 # Размер анализируемой сетки
 SIZE_SEARCH_STEP_1 = 50
@@ -27,9 +27,13 @@ SIZE_SEARCH_STEP_4 = 1
 
 
 def fmt(x, pos):
+    '''
+    Функция что бы у графика была в шкала в "10 в степени"
+    '''
     a, b = '{:.2e}'.format(x).split('e')
     b = int(b)
     return r'${} \times 10^{{{}}}$'.format(a, b)
+
 
 class WorkerSignals(QObject):
     finished = Signal()
@@ -46,109 +50,94 @@ class Worker(QRunnable):
         self.obj_type = obj_type
         self.power = power
 
+    def search_nearby(self, x_min: int, x_max: int, y_min: int, y_max: int, size_search: int,
+                      object_under_study) -> list:
+        '''
+        Функция поиска близлежащих координат
+        :param x_min: - мин.координата области поиска по Х
+        :param x_max: - макс.координата области поиска по Х
+        :param y_min: - мин.координата области поиска по Y
+        :param y_max: - макс.координата области поиска по Y
+        :param size_search: - размер квадратов на которые происходит разбивка
+        :param object_under_study: - исследуемый объект (LineString, Polygon, Point)
+        :return: result_square - спосок координат близлежащих к объекту квадратов
+        '''
+        x_line = [i for i in range(x_min, x_max + 1, size_search)]
+        y_line = [i for i in range(y_min, y_max + 1, size_search)]
+        result_square = []
+        for x in x_line:
+            for y in y_line:
+                search_point = (x, y, x + size_search, y, x + size_search, y + size_search, x, y + size_search)
+                search_polygon = Polygon(list(zip(search_point[0::2], search_point[1::2])))
+                distance = search_polygon.distance(object_under_study)
+                if distance < self.power:
+                    result_square.extend(search_point)
+        return result_square
+
     def run(self):
         try:
-            # создадим силу
+            # создадим силу воздействия
             dist_power = [i for i in range(self.power)]
             power = list(reversed([i / 100 for i in dist_power]))
-            print(dist_power)
-            print(power)
             # нулевая матрица
             zeors_array = np.zeros((self.size_area[0], self.size_area[1]))
             # Создадим объект до которого идет измерение
             if len(self.obj_points) > 2:
-                if self.obj_type == 0:  # линейный объект
-                    # print('линейный')
+                if self.obj_type == 0:  # линия
                     obj = LineString(list(zip(self.obj_points[0::2], self.obj_points[1::2])))
-                else:
-                    # print('стационарный')
+                else:  # многоугольник
                     obj = Polygon(list(zip(self.obj_points[0::2], self.obj_points[1::2])))
-            else:
-                # print('точка')
+            else:  # точка
                 obj = Point(self.obj_points[0], self.obj_points[1])
-            # посмотрим к каким областям относится рассматриваемы объект
-            # разобьем площадь картинки на квадраты поиска на квадраты поиска
-            x_line = [i for i in range(0, self.size_area[0] + 1, SIZE_SEARCH_STEP_1)]
-            y_line = [i for i in range(0, self.size_area[1] + 1, SIZE_SEARCH_STEP_1)]
+            # ШАГ 1.
+            # разобъем область на квадраты по 50 пикселей и определим близлежащие к объекту
+            find_square_50 = self.search_nearby(x_min=0, x_max=self.size_area[0], y_min=0, y_max=self.size_area[1],
+                                                size_search=SIZE_SEARCH_STEP_1, object_under_study=obj)
+            # ШАГ 2.
+            find_square_10 = []
+            for i in range(0, len(find_square_50), 8):
+                # Получим координаты квадратов по 50 пикселей и снова их разобъем на
+                # квадраты по 10 пикселей и определим близлежащие
+                sq = find_square_50[i:i + 8]
 
-            find_coordinate_step_1 = []
+                # разобьем квадраты 50 на квадраты по 10
+                result_square = self.search_nearby(x_min=sq[0], x_max=sq[2], y_min=sq[1], y_max=sq[5],
+                                                   size_search=SIZE_SEARCH_STEP_2, object_under_study=obj)
+                find_square_10.extend(result_square)
 
-            for x in x_line:
-                for y in y_line:
-                    search_point = (x, y, x + SIZE_SEARCH_STEP_1, y, x + SIZE_SEARCH_STEP_1, y + SIZE_SEARCH_STEP_1, x,
-                                    y + SIZE_SEARCH_STEP_1)
-                    search_polygon = Polygon(list(zip(search_point[0::2], search_point[1::2])))
-                    distance = search_polygon.distance(obj)
-                    if distance < self.power:
-                        find_coordinate_step_1.extend(search_point)
-
-            # print(f'Координаты 1: {find_coordinate_step_1}')
-            find_coordinate_step_2 = []
-            for i in range(0, len(find_coordinate_step_1), 8):
-                sq = find_coordinate_step_1[i:i + 8]
-                # разобьем площадь картинки на квадраты поиска на квадраты поиска
-                x_line = [i for i in range(sq[0], sq[2] + 1, SIZE_SEARCH_STEP_2)]
-                y_line = [i for i in range(sq[1], sq[5] + 1, SIZE_SEARCH_STEP_2)]
-                for x in x_line:
-                    for y in y_line:
-                        search_point = (
-                            x, y, x + SIZE_SEARCH_STEP_2, y, x + SIZE_SEARCH_STEP_2, y + SIZE_SEARCH_STEP_2, x,
-                            y + SIZE_SEARCH_STEP_2)
-                        search_polygon = Polygon(list(zip(search_point[0::2], search_point[1::2])))
-                        distance = search_polygon.distance(obj)
-                        if distance < self.power:
-                            find_coordinate_step_2.extend(search_point)
-
-            # print(f'Координаты 2: {find_coordinate_step_2}')
-            find_coordinate_step_3 = []
-            for i in range(0, len(find_coordinate_step_2), 8):
-                sq = find_coordinate_step_2[i:i + 8]
-                # разобьем площадь картинки на квадраты поиска на квадраты поиска
-                x_line = [i for i in range(sq[0], sq[2] + 1, SIZE_SEARCH_STEP_3)]
-                y_line = [i for i in range(sq[1], sq[5] + 1, SIZE_SEARCH_STEP_3)]
-                for x in x_line:
-                    for y in y_line:
-                        search_point = (
-                            x, y, x + SIZE_SEARCH_STEP_3, y, x + SIZE_SEARCH_STEP_3, y + SIZE_SEARCH_STEP_3, x,
-                            y + SIZE_SEARCH_STEP_3)
-                        search_polygon = Polygon(list(zip(search_point[0::2], search_point[1::2])))
-                        distance = search_polygon.distance(obj)
-                        if distance < self.power:
-                            find_coordinate_step_3.extend(search_point)
-            # print(f'Координаты 3: {find_coordinate_step_3}')
-
+            # ШАГ 3.
+            find_square_2 = []
+            for i in range(0, len(find_square_10), 8):
+                # Получим координаты квадратов по 10 пикселей и снова их разобъем на
+                # квадраты по 2 пикселя и определим близлежащие
+                sq = find_square_10[i:i + 8]
+                # разобьем квадраты 10 на квадраты по 2
+                result_square = self.search_nearby(x_min=sq[0], x_max=sq[2], y_min=sq[1], y_max=sq[5],
+                                                   size_search=SIZE_SEARCH_STEP_3, object_under_study=obj)
+                find_square_2.extend(result_square)
+            # # ШАГ 4.
             find_coordinate_step_4 = []
-            for i in range(0, len(find_coordinate_step_3), 8):
-                sq = find_coordinate_step_3[i:i + 8]
-                # разобьем площадь картинки на квадраты поиска на квадраты поиска
+            for i in range(0, len(find_square_2), 8):
+                sq = find_square_2[i:i + 8]
+                # Пройдемся по квадратам find_square_2 и опредлим какие точки в них
+                # будут близлежащими к объекту
                 x_line = [i for i in range(sq[0], sq[2] + 1, SIZE_SEARCH_STEP_4)]
                 y_line = [i for i in range(sq[1], sq[5] + 1, SIZE_SEARCH_STEP_4)]
-                # print(x_line)
-                # print(y_line)
-                # print(20*'-')
                 for x in x_line:
                     for y in y_line:
-                        # print(f'x = {x}, y = {y}')
                         search_point = Point(x, y)
                         find_coordinate_step_4.append(search_point)
-
-
-            i =0
-            l = len(list(set(find_coordinate_step_4)))
+            # Удалим повторы точек и посчитаем в какой точке какое воздействие
             for item in list(set(find_coordinate_step_4)):
                 distance = int(item.distance(obj))
-                print(f'distance = {distance}, i={i}, l={l}')
-                i+=1
                 if distance == 0:
                     zeors_array[int(item.x), int(item.y)] = max(power)
                 else:
                     if int(distance) in dist_power:
-                        ind = dist_power.index(int(distance)-1)
+                        ind = dist_power.index(int(distance) - 1)
                         zeors_array[int(item.x), int(item.y)] = power[ind]
                     if distance == 0:
                         zeors_array[int(item.x), int(item.y)] = max(power)
-
-
 
         except Exception as e:
             self.signals.error.emit(str(e))
@@ -188,7 +177,7 @@ class MainWindow(QMainWindow):
             plt.show()
 
     def worker_complete(self):
-        print("THREAD COMPLETE!")
+        print("Поток завершен!")
 
 
 if __name__ == '__main__':
@@ -197,6 +186,4 @@ if __name__ == '__main__':
     window.show()
     app.exec()
 
-# plt.pcolor(result, cmap='jet')
-# plt.colorbar(format=ticker.FuncFormatter(fmt))
-# plt.show()
+
